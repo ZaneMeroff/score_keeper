cf437106da6c73f496c90399a59679a8cf2168c6
ZaneMeroff <zanemeroff@gmail.com>
2021-06-24 09:46:55 -0600

    feat(uuid): add package with types

--- src/store/Scores.ts
+++ src/store/Scores.ts
@@ -3,13 +3,22 @@ import { Action, Module, Mutation, VuexModule } from 'vuex-class-modules'
 
 // Types
 import { Score, ScoreData } from '@/types/scores'
+import Vue from 'vue'
 
 @Module
 export default class Scores extends VuexModule {
 
     scoreData: ScoreData = {}
 
+    get getScoreData() {
+      return this.scoreData
+    }
+
+    @Mutation
+    setScoreData(payload: { id: string, updatedScore: number }) {
+      // create or update score data based on id
+      Vue.set(this.scoreData, payload.id, payload.updatedScore)
+    }
+
     
-
-
 }
